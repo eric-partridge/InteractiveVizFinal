f7c633687d1d58ef647ea6f697599c4dd75ced80
Unknown <castrj5@rpi.edu>
2020-04-15 18:14:20 -0400

    data setup is getting there

---
+++ New Unity Project/Assets/DataParse.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using FileHelpers;
+
+[DelimitedRecord(",")]
+public class Stock
+{
+	public string Symbol;
+
+	public string Date;
+
+	public float Value;
+}
+
+public class DataParse : MonoBehaviour
+{
+    public string current_directory;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        ParseAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ParseAll()
+    {
+        var engine = new FileHelperEngine<Stock>();
+        current_directory = Directory.GetCurrentDirectory();
+        current_directory += "\\Data";
+        string[] files = Directory.GetFiles(current_directory);
+        foreach (string file in files)
+        {
+            print(file);
+            var result = engine.ReadFile("file");
+        }
+    }
+}
