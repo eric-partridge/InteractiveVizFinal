54e81ffbb642df74f2b9768204b8121dc9499fad
Unknown <castrj5@rpi.edu>
2020-04-20 20:17:40 -0400

    button spawning

--- New Unity Project/Assets/ButtonSpawn.cs
+++ New Unity Project/Assets/ButtonSpawn.cs
@@ -6,17 +6,37 @@ public class ButtonSpawn : MonoBehaviour
 {
     public GameObject button;
     public GameObject treeBox;
+    public float spacing = 25;
+    private float right = -400;
+    private float up = 300;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Spawn();
+        }
+    }
+
+    public void Spawn()
+    {
+        GameObject b = Instantiate(button);
+        b.transform.parent = treeBox.transform;
+        RectTransform r = b.GetComponent<RectTransform>();
+        r.localScale = Vector3.one;
+        r.localPosition = new Vector3(right, up, 0);
+        up -= 50 + spacing;
+        if(up < -300)
+        {
+            up = 300;
+            right += 200 + spacing;
+        }
     }
 }
